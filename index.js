9fdfeda30a4a6ec1aaf5cef31af6ae2339b39548
George Luther <george_l@thinkful.com>
2020-11-24 11:24:50 -0500

    basic functionality, need to add specific queries RE stateCode etc, next

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const apiKey = 'RSbogVFjSZx2bnthROdrBKsaeExwL5wSrLlWcPNp'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    console.log(queryItems.join('&'))
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the items array
+  for (let i = 0; i < responseJson.data.length; i++){
+    // for each video object in the items 
+    //array, add a list item to the results 
+    //list with the video title, description,
+    //and thumbnail
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <img src=${responseJson.data[i].images[0].url} width=400px}'>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getParks(query, limit=10) {
+  const params = {
+    q: query,
+    limit,
+    api_key: apiKey
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const limit = $('#js-max-results').val();
+    getParks(searchTerm, limit);
+  });
+}
+
+$(watchForm);
